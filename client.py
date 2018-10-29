4dec052c555b207e81e1d0283ea4e20a8557e784
sergiomc95 <sergiomartinc95@gmail.com>
2018-10-29 19:01:16 +0100

    subo el ejercicio 4 sin pep8

--- client.py
+++ client.py
@@ -5,17 +5,25 @@ Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = 'localhost'
-PORT = 6001
-LINE = '¡Hola mundo!'
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    MESSAGE = sys.argv[3]
 
+    if len(sys.argv) > 3:
+        MESSAGE = ''
+        CADENA = sys.argv[3:]
+        for characters in CADENA:
+            MESSAGE += ' ' + characters
+except IndexError:
+    sys.exit(" Usage: python3 client.py <server> <port> <message>")
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", MESSAGE)
+    my_socket.send(bytes(MESSAGE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
