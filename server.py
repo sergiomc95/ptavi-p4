7d14b94b558499a6acc9d0038f36d7439079bd49
sergiomc95 <sergiomartinc95@gmail.com>
2018-10-30 15:45:58 +0100

    ejercicio5 sin pep8

--- server.py
+++ server.py
@@ -5,8 +5,12 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
-
+try:
+    PORT = sys.argv[1]
+except IndexError:
+    sys.exit("Usage: python3 server.py <port>")
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
@@ -18,13 +22,14 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         (all requests will be handled by this method)
         """
         self.wfile.write(b"Hemos recibido tu peticion")
+        print(self.client_address)
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', int(PORT)), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
