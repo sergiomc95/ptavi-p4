5bf007ec85ec5db8a726acf292e98dfb16e815ad
sergiomc95 <sergiomartinc95@gmail.com>
2018-11-02 18:11:08 +0100

    ejercicio6 pep8

--- server.py
+++ server.py
@@ -8,30 +8,39 @@ import socketserver
 import sys
 
 try:
-    PORT = sys.argv[1]
+    PORT = int(sys.argv[1])
 except IndexError:
     sys.exit("Usage: python3 server.py <port>")
-class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+
+
+class SIPRegistrerHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
-        """
-        handle method of the server class
-        (all requests will be handled by this method)
-        """
-        self.wfile.write(b"Hemos recibido tu peticion")
-        print(self.client_address)
+        IP = self.client_address[0]
+        PORT = self.client_address[1]
+        Users = {'User: ', 'IP: '}
+        print("Client_IP: ", IP + "\t", "Client_Port: ", PORT)
+        self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
+            line = line.decode('utf-8')
+            line = line.split(' ')
+            Method = line[0]
+            if Method == 'REGISTER':
+                User = line[1].split(':')[1]
+                SIP = line[-1]
+                Users['IP'] = IP
+                Users['User'] = User
+                print(Users)
+
 
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', int(PORT)), EchoHandler)
+    serv = socketserver.UDPServer(('', PORT), SIPRegistrerHandler)
 
-    print("Lanzando servidor UDP de eco...")
+    print("\n" + "Lanzando servidor UDP de eco LOCAL - PUERTO: " +
+          str(PORT) + "\n")
     try:
         serv.serve_forever()
     except KeyboardInterrupt:
