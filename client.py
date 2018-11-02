70239d83969e180730625bb9b94f8a2c1ac5533b
sergiomc95 <sergiomartinc95@gmail.com>
2018-11-02 18:11:45 +0100

    ejercicio6 pep8

--- client.py
+++ client.py
@@ -10,21 +10,25 @@ import sys
 try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
-    MESSAGE = sys.argv[3]
+    METHOD = sys.argv[3]
+    USER = sys.argv[4]
 
-    if len(sys.argv) > 3:
-        MESSAGE = ''
-        CADENA = sys.argv[3:]
-        for characters in CADENA:
-            MESSAGE += ' ' + characters
+
 except IndexError:
-    sys.exit(" Usage: python3 client.py <server> <port> <message>")
+    sys.exit(" Usage: python3 client.py <server> <method> <email>")
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", MESSAGE)
-    my_socket.send(bytes(MESSAGE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
 
-print("Socket terminado.")
+    my_socket.connect((SERVER, PORT))
+
+    if METHOD == "register" or METHOD == "REGISTER":
+
+        METHOD = "REGISTER"
+        line = METHOD + " sip:" + str(USER) + " SIP/2.0\r\n\r\n"
+        print("Enviando:", line)
+        my_socket.send(bytes(line, 'utf-8'))
+        data = my_socket.recv(1024)
+        print(data.decode('utf-8'))
+    else:
+        sys.exit(" Usage: python3 client.py <server> <method> <meail>")
+print("Socket terminado. \r\n")
